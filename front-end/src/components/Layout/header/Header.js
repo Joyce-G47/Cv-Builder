32cf1b2c92cca9d52d5636b4397181e90f9a4c2a
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-08-21 09:16:36 +0200

    added font icons and small screen size

--- front-end/src/components/Layout/header/Header.js
+++ front-end/src/components/Layout/header/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="headerpage">
       <header className="header">
@@ -10,30 +16,42 @@ const Header = () => {
             <div className="logo">
               <h1>CV Builder</h1>
             </div>
-            <nav className="nav">
+            <div className="menu-icon" onClick={toggleMenu}>
+              <i className="fas fa-bars"></i>
+            </div>
+            <nav className={`nav ${isMenuOpen ? "nav-open" : ""}`}>
               <ul>
                 <li>
-                  <a href="#Home">Home</a>
+                  <a href="#Home">
+                    <i className="fas fa-home"></i> Home
+                  </a>
                 </li>
                 <li>
-                  <a href="#CVLetter">CV Builder</a>
+                  <a href="#CVLetter">
+                    <i className="fas fa-file-alt"></i> CV Builder
+                  </a>
                 </li>
                 <li>
-                  <a href="#CoverLetter">Cover Letter</a>
+                  <a href="#CoverLetter">
+                    <i className="fas fa-envelope"></i> Cover Letter
+                  </a>
                 </li>
                 <li>
-                  <a href="#contact-us">Contact</a>
+                  <a href="#contact-us">
+                    <i className="fas fa-phone"></i> Contact
+                  </a>
                 </li>
-
                 <li className="login-button">
-                <button>Login</button>
+                  <button>
+                    <i className="fas fa-sign-in-alt"></i> Login
+                  </button>
                 </li>
                 <li className="signup-button">
-                <button>SignUp</button>
+                  <button>
+                    <i className="fas fa-user-plus"></i> SignUp
+                  </button>
                 </li>
-               
               </ul>
-             
             </nav>
           </div>
         </div>
